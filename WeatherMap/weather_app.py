b117ab3706adf95067d69e60184841f7fc18cd35
leni322 <malovik.m@mail.ru>
2024-08-16 11:07:45 +0300

    Added a database to save

--- WeatherMap/weather_app.py
+++ WeatherMap/weather_app.py
@@ -1,24 +1,39 @@
 import requests
 import sys
+import sqlite3
+import os
 from config import API_KEY, BASE_URL
+from datetime import datetime
 
+
+db_path = os.path.join(os.getcwd(), 'weather_history.db')
+
+def init_db():
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS history (
+            id INTEGER PRIMARY KEY,
+            timestamp TEXT,
+            city_name TEXT,
+            weather_info TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
 def get_weather(city_name):
-    # Формируем полный URL с параметрами
     url = f"{BASE_URL}?q={city_name}&appid={API_KEY}&units=metric&lang=ru"
-
-    # Делаем запрос к API
     response = requests.get(url)
-
-    # Проверяем статус-код ответа
     if response.status_code == 200:
         data = response.json()
-        return format_weather_data(data)  # Передаем data в функцию
+        weather_info = format_weather_data(data)
+        save_to_db(city_name, weather_info)
+        return weather_info
     elif response.status_code == 404:
         return f"Город '{city_name}' не найден. Пожалуйста, проверьте название и попробуйте снова."
     else:
         return f"Ошибка при запросе данных. Код ошибки: {response.status_code}"
-
 
 def format_weather_data(data):
     city = data['name']
@@ -38,8 +53,19 @@ def format_weather_data(data):
 
     return weather_report
 
+def save_to_db(city_name, weather_info):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    cursor.execute('''INSERT INTO history (timestamp, city_name, weather_info)
+        VALUES (?, ?, ?)
+    ''', (timestamp, city_name, weather_info))
+    conn.commit()
+    conn.close()
 
 def main():
+    init_db()  # Инициализация базы данных при запуске программы
+
     while True:
         city_name = input("Введите название города (или 'выход' для завершения): ")
 
@@ -49,7 +75,6 @@ def main():
 
         weather = get_weather(city_name)
         print(weather)
-
 
 if __name__ == "__main__":
     main()
